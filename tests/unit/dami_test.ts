c6873e82b453e981d57eaafce23950a0baa1d17f
Edward Bebbington <edward.bebbington@intercity.technology>
2020-08-13 18:33:52 +0100

    deno fmt

--- tests/unit/dami_test.ts
+++ tests/unit/dami_test.ts
@@ -1,32 +1,32 @@
 import { Rhum } from "../deps.ts";
-import {DAMI} from "../../src/dami.ts";
+import { DAMI } from "../../src/dami.ts";
 
 const ami = {
   hostname: "0.0.0.0",
   port: 5038,
-  logger: false
-}
+  logger: false,
+};
 
 const auth = {
   username: "admin",
-  secret: "mysecret"
-}
+  secret: "mysecret",
+};
 
 Rhum.testPlan("tests/unit/dami.ts", () => {
   Rhum.testSuite("close()", () => {
     Rhum.testCase("Closes the connection", async () => {
-      const Dami = new DAMI(ami)
-      await Dami.connectAndLogin(auth)
-      Dami.close()
+      const Dami = new DAMI(ami);
+      await Dami.connectAndLogin(auth);
+      Dami.close();
     });
   });
   Rhum.testSuite("connectAndLogin()", () => {
     Rhum.testCase("Throws an error when `conn` is already set", () => {
     });
     Rhum.testCase("Successfully connects and logs in to the AMI", async () => {
-      const Dami = new DAMI(ami)
-      await Dami.connectAndLogin(auth)
-      Dami.close()
+      const Dami = new DAMI(ami);
+      await Dami.connectAndLogin(auth);
+      Dami.close();
     });
   });
   Rhum.testSuite("to()", () => {
