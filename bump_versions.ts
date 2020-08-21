f40022bd12ce86cd5c78983c11fa9627d00c949f
Edward Bebbington <edward.bebbington@intercity.technology>
2020-08-21 09:05:19 +0100

    bump versions

--- bump_versions.ts
+++ bump_versions.ts
@@ -3,6 +3,6 @@ const version = branch.substring(branch.indexOf("v") + 1); // 1.0.5
 let eggsContent = new TextDecoder().decode(await Deno.readFile("./egg.json"));
 eggsContent = eggsContent.replace(
   /"version": "[0-9\.]+[0-9\.]+[0-9\.]"/,
-  `"version: "${version}"`,
+  `"version": "${version}"`,
 );
-//await Deno.writeFile("./egg.json", new TextEncoder().encode(eggsContent));
+await Deno.writeFile("./egg.json", new TextEncoder().encode(eggsContent));
