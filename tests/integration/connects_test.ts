f6bcb1fd284379ab9bb895bff3bf775717a33530
Edward Bebbington <edward.bebbington@intercity.technology>
2020-08-21 07:56:55 +0100

    update connect test

--- tests/integration/connects_test.ts
+++ tests/integration/connects_test.ts
@@ -1,58 +1,18 @@
 import { DAMI, DAMIData } from "../../src/dami.ts";
 import { Rhum } from "../deps.ts";
-
-const ami = {
-  hostname: "0.0.0.0",
-  port: 5038,
-  logger: false,
-};
-
-const auth = {
-  username: "admin",
-  secret: "mysecret",
-};
-
-function sleep(milliseconds: number) {
-  const start = new Date().getTime();
-  for (let i = 0; i < 1e7; i++) {
-    if ((new Date().getTime() - start) > milliseconds) {
-      break;
-    }
-  }
-}
+import { auth, ami } from "../utils.ts"
 
 Deno.test({
-  name: "Can connect, login and receieve/send events",
+  name: "Can connect and login",
   sanitizeOps: false,
   sanitizeResources: false,
   async fn(): Promise<void> {
     // Can connect and login
     const Dami = new DAMI(ami);
     await Dami.connectAndLogin(auth);
-    // Can receive events
-    // deno-lint-ignore no-explicit-any
-    let res: any = {};
-    // deno-lint-ignore no-explicit-any
-    const peerEntryResults: any = [];
-    Dami.on("FullyBooted", (data) => {
-      res = data;
-    });
-    Dami.on("PeerEntry", (data) => {
-      peerEntryResults.push(data);
-    });
     await Dami.listen();
-    await setTimeout(() => {
-      Rhum.asserts.assertEquals(res.Event, "FullyBooted");
-      Rhum.asserts.assertEquals(res.Message, "Authentication accepted");
-    }, 2000);
-
-    // can send/trigger events
-    await Dami.to("SIPPeers", {});
-    await setTimeout(() => {
-      Rhum.asserts.assertEquals(peerEntryResults.length, 2);
-      Rhum.asserts.assertEquals(peerEntryResults[0].ObjectName, 6001);
-      Rhum.asserts.assertEquals(peerEntryResults[1].ObjectName, 6002);
-      Dami.close();
-    }, 2000);
+    const pong = await Dami.ping()
+    Rhum.asserts.assertEquals(pong, true)
+    Dami.close()
   },
 });
