4dd94d1fbc97b696e4dd6b7b30ff323363903653
Edward Bebbington <edward.bebbington@intercity.technology>
2020-08-21 07:57:31 +0100

    update valid data integration test

--- tests/integration/returns_valid_data_test.ts
+++ tests/integration/returns_valid_data_test.ts
@@ -1,25 +1,6 @@
 import { DAMI, DAMIData } from "../../src/dami.ts";
 import { Rhum } from "../deps.ts";
-
-const ami = {
-  hostname: "0.0.0.0",
-  port: 5038,
-  logger: false,
-};
-
-const auth = {
-  username: "admin",
-  secret: "mysecret",
-};
-
-function sleep(milliseconds: number) {
-  var start = new Date().getTime();
-  for (var i = 0; i < 1e7; i++) {
-    if ((new Date().getTime() - start) > milliseconds) {
-      break;
-    }
-  }
-}
+import {ami, auth} from "../utils.ts";
 
 Deno.test({
   name: "Returns the expected data on a event response",
@@ -34,13 +15,9 @@ Deno.test({
       fullyBootedRes = data;
     });
     let peerEntryRes: Array<DAMIData> = [];
-    Dami.on("PeerEntry", (data) => {
-      peerEntryRes.push(data);
-    });
-    Dami.to("SIPPeers", {});
+    peerEntryRes = await Dami.to("SIPPeers", {ActionID: 12});
     await setTimeout(() => {
       // Check response data has ONLY the set amount of properties, and assert that data in those properties
-      //console.log(fullyBootedRes)
       const numberOfProps = Object.keys(fullyBootedRes).length;
       Rhum.asserts.assertEquals(numberOfProps, 7); // "FullyBooted" returns 7 props
       Rhum.asserts.assertEquals(fullyBootedRes.Event, "FullyBooted");
@@ -52,32 +29,16 @@ Deno.test({
         "Authentication accepted",
       );
       // And ensure things that trigger multiple events are correct
-      Rhum.asserts.assertEquals(peerEntryRes.length, 2);
+      Rhum.asserts.assertEquals(peerEntryRes.length, 4);
       const [numberOfProps2, numberOfProps3] = [
-        Object.keys(peerEntryRes[0]).length,
         Object.keys(peerEntryRes[1]).length,
+        Object.keys(peerEntryRes[2]).length,
       ];
-      Rhum.asserts.assertEquals(numberOfProps2, 16);
-      Rhum.asserts.assertEquals(numberOfProps3, 16);
-      Rhum.asserts.assertEquals(peerEntryRes[0].Event, "PeerEntry");
-      Rhum.asserts.assertEquals(peerEntryRes[0].Channeltype, "SIP");
-      Rhum.asserts.assertEquals(peerEntryRes[0].ObjectName, 6001);
-      Rhum.asserts.assertEquals(peerEntryRes[0].ChanObjectType, "peer");
-      Rhum.asserts.assertEquals(peerEntryRes[0].IPaddress, "-none-");
-      Rhum.asserts.assertEquals(peerEntryRes[0].IPport, 0);
-      Rhum.asserts.assertEquals(peerEntryRes[0].Dynamic, "yes");
-      Rhum.asserts.assertEquals(peerEntryRes[0].AutoForcerport, "yes");
-      Rhum.asserts.assertEquals(peerEntryRes[0].Forcerport, "no");
-      Rhum.asserts.assertEquals(peerEntryRes[0].AutoComedia, "no");
-      Rhum.asserts.assertEquals(peerEntryRes[0].Comedia, "no");
-      Rhum.asserts.assertEquals(peerEntryRes[0].VideoSupport, "no");
-      Rhum.asserts.assertEquals(peerEntryRes[0].TextSupport, "no");
-      Rhum.asserts.assertEquals(peerEntryRes[0].ACL, "no");
-      Rhum.asserts.assertEquals(peerEntryRes[0].Status, "Unmonitored");
-      Rhum.asserts.assertEquals(peerEntryRes[0].RealtimeDevice, "no");
+      Rhum.asserts.assertEquals(numberOfProps2, 17);
+      Rhum.asserts.assertEquals(numberOfProps3, 17);
       Rhum.asserts.assertEquals(peerEntryRes[1].Event, "PeerEntry");
       Rhum.asserts.assertEquals(peerEntryRes[1].Channeltype, "SIP");
-      Rhum.asserts.assertEquals(peerEntryRes[1].ObjectName, 6002);
+      Rhum.asserts.assertEquals(peerEntryRes[1].ObjectName, 6001);
       Rhum.asserts.assertEquals(peerEntryRes[1].ChanObjectType, "peer");
       Rhum.asserts.assertEquals(peerEntryRes[1].IPaddress, "-none-");
       Rhum.asserts.assertEquals(peerEntryRes[1].IPport, 0);
@@ -91,6 +52,22 @@ Deno.test({
       Rhum.asserts.assertEquals(peerEntryRes[1].ACL, "no");
       Rhum.asserts.assertEquals(peerEntryRes[1].Status, "Unmonitored");
       Rhum.asserts.assertEquals(peerEntryRes[1].RealtimeDevice, "no");
+      Rhum.asserts.assertEquals(peerEntryRes[2].Event, "PeerEntry");
+      Rhum.asserts.assertEquals(peerEntryRes[2].Channeltype, "SIP");
+      Rhum.asserts.assertEquals(peerEntryRes[2].ObjectName, 6002);
+      Rhum.asserts.assertEquals(peerEntryRes[2].ChanObjectType, "peer");
+      Rhum.asserts.assertEquals(peerEntryRes[2].IPaddress, "-none-");
+      Rhum.asserts.assertEquals(peerEntryRes[2].IPport, 0);
+      Rhum.asserts.assertEquals(peerEntryRes[2].Dynamic, "yes");
+      Rhum.asserts.assertEquals(peerEntryRes[2].AutoForcerport, "yes");
+      Rhum.asserts.assertEquals(peerEntryRes[2].Forcerport, "no");
+      Rhum.asserts.assertEquals(peerEntryRes[2].AutoComedia, "no");
+      Rhum.asserts.assertEquals(peerEntryRes[2].Comedia, "no");
+      Rhum.asserts.assertEquals(peerEntryRes[2].VideoSupport, "no");
+      Rhum.asserts.assertEquals(peerEntryRes[2].TextSupport, "no");
+      Rhum.asserts.assertEquals(peerEntryRes[2].ACL, "no");
+      Rhum.asserts.assertEquals(peerEntryRes[2].Status, "Unmonitored");
+      Rhum.asserts.assertEquals(peerEntryRes[2].RealtimeDevice, "no");
       Dami.close();
     }, 2000);
   },
