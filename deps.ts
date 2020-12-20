95af630327b7a234a94f641ec2f2e8d03cf73e8f
ebebbington <ebebbington@users.noreply.github.com>
2020-12-20 00:21:36 +0000

    chore: Update dependencies

--- deps.ts
+++ deps.ts
@@ -1,1 +1,1 @@
-export { readStringDelim } from "https://deno.land/std@0.78.0/io/mod.ts";
+export { readStringDelim } from "https://deno.land/std@0.81.0/io/mod.ts";
--- tests/deps.ts
+++ tests/deps.ts
@@ -1,3 +1,3 @@
-export { Rhum } from "https://deno.land/x/rhum@v1.1.4/mod.ts";
+export { Rhum } from "https://deno.land/x/rhum@v1.1.5/mod.ts";
 
-export { deferred } from "https://deno.land/std@0.77.0/async/deferred.ts";
+export { deferred } from "https://deno.land/std@0.81.0/async/deferred.ts";
