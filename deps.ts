d4342b6ef0cd6b1ae7d087b4bbb7e9861dc79906
ebebbington <ebebbington@users.noreply.github.com>
2021-01-24 00:51:51 +0000

    chore: Update dependencies

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
-export { readStringDelim } from "https://deno.land/std@0.83.0/io/mod.ts";
-export { deferred } from "https://deno.land/std@0.83.0/async/deferred.ts";
-export type { Deferred } from "https://deno.land/std@0.83.0/async/deferred.ts";
+export { readStringDelim } from "https://deno.land/std@0.84.0/io/mod.ts";
+export { deferred } from "https://deno.land/std@0.84.0/async/deferred.ts";
+export type { Deferred } from "https://deno.land/std@0.84.0/async/deferred.ts";
