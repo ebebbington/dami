0069c872ca616932178ab9836fa77952aeda0691
Edward Bebbington <EdwardSBebbington@hotmail.com>
2021-12-07 13:17:27 +0000

    fix err

--- src/dami.ts
+++ src/dami.ts
@@ -295,7 +295,7 @@ export class DAMI {
           }
         }
       } catch (err) {
-        if (err.message.indexOf("Bad resource ID") > -1) {
+        if (err instanceof Deno.errors.BadResource || err instanceof Deno.errors.Interrupted) {
           this.connected = false;
           return;
         }
