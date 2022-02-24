230ba5b105e09b5abb427b884810f7c5e5d36a14
Edward Bebbington <47337480+ebebbington@users.noreply.github.com>
2022-02-24 00:07:14 +0000

    chore: Update dependencies (#82)
    Co-authored-by: ebebbington <ebebbington@users.noreply.github.com>

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
-export { readStringDelim } from "https://deno.land/std@0.123.0/io/mod.ts";
-export { deferred } from "https://deno.land/std@0.123.0/async/deferred.ts";
-export type { Deferred } from "https://deno.land/std@0.123.0/async/deferred.ts";
+export { readStringDelim } from "https://deno.land/std@0.126.0/io/mod.ts";
+export { deferred } from "https://deno.land/std@0.126.0/async/deferred.ts";
+export type { Deferred } from "https://deno.land/std@0.126.0/async/deferred.ts";
--- tests/deps.ts
+++ tests/deps.ts
@@ -2,4 +2,4 @@ export {
   assertEquals,
   assertRejects,
   assertThrows,
-} from "https://deno.land/std@0.123.0/testing/asserts.ts";
+} from "https://deno.land/std@0.126.0/testing/asserts.ts";
